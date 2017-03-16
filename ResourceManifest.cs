af7edd4db3dc54f95bde08af26110f8b9ec39691
Márk Bartha <barthamark@gmail.com>
2017-03-17 00:35:53 +0100

    Adding existing code files of the blog theme

---
+++ ResourceManifest.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.UI.Resources;
+
+namespace MarkBartha.BasicBlogTheme
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+        }
+    }
+}
